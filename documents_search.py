bcad0af9b24ad0ed8d08f79fd4e284181dc1203f
Krzysztof Urban <krz.urb@gmail.com>
2011-10-10 01:33:00 +0200

    application of stopwords

--- documents_search.py
+++ documents_search.py
@@ -8,17 +8,31 @@ stemmer = PorterStemmer()
 
 def clean_word(word):
     word = word.lower()
-    return stemmer.stem(word, 0, len(word)-1)
+    if word in stopwords:
+        return None
+    else:
+        return stemmer.stem(word, 0, len(word)-1)
 
+def clean_word_list(list):
+    return [word for word in list if word is not None]
+
+stopwords_filepath = "data/stopwords.txt"
 keywords_filepath = "data/keywords.txt"
 documents_filepath = "data/documents.txt"
+
+stopwords = []
+with open(stopwords_filepath) as file:
+    for line in file:
+        stopwords.append(line.strip())
+stopwords = frozenset(stopwords)
 
 keywords = OrderedDict()
 with open(keywords_filepath) as file:
     for line in file:
         word = line.strip()
         stemmed_word = clean_word(word)
-        keywords[stemmed_word] = 0
+        if stemmed_word is not None:
+            keywords[stemmed_word] = 0
 
 for k in sorted(keywords):
     print k
@@ -35,7 +49,7 @@ with open(documents_filepath) as file:
 
 for k, doc in documents.iteritems():
     clean_words = map(clean_word, doc)
-    documents[k] = clean_words
+    documents[k] = clean_word_list(clean_words)
 
 for doc in documents.items():
     print doc
