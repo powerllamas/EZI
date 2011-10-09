d102ee0bee163fe1af25fe7f9f116a862259744a
Krzysztof Urban <krz.urb@gmail.com>
2011-10-10 00:17:42 +0200

    word cleaning for docs

--- documents_search.py
+++ documents_search.py
@@ -4,16 +4,20 @@ from collections import OrderedDict, defaultdict
 
 from stemmer import PorterStemmer
 
+stemmer = PorterStemmer()
+
+def clean_word(word):
+    word = word.lower()
+    return stemmer.stem(word, 0, len(word)-1)
+
 keywords_filepath = "data/keywords.txt"
 documents_filepath = "data/documents.txt"
-
-stemmer = PorterStemmer()
 
 keywords = OrderedDict()
 with open(keywords_filepath) as file:
     for line in file:
         word = line.strip()
-        stemmed_word = stemmer.stem(word, 0, len(word)-1)
+        stemmed_word = clean_word(word)
         keywords[stemmed_word] = 0
 
 for k in sorted(keywords):
@@ -28,6 +32,10 @@ with open(documents_filepath) as file:
             documents[current_doc] += words
         else:
             current_doc += 1
+
+for k, doc in documents.iteritems():
+    clean_words = map(clean_word, doc)
+    documents[k] = clean_words
 
 for doc in documents.items():
     print doc
