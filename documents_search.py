943155868723c7d32adcf596990d101d59094e2f
Tomek <tomek.zietkiewicz@gmail.com>
2011-10-11 22:23:54 +0200

    Searching works somehow, need some refactoring, testing and GUI

--- documents_search.py
+++ documents_search.py
@@ -1,58 +1,155 @@
 # -*- coding: utf-8 -*-
 
 from collections import OrderedDict, defaultdict
-
 from stemmer import PorterStemmer
+import math
+import sys
 
-stemmer = PorterStemmer()
 
-def clean_word(word):
-    word = word.lower()
-    word = filter(lambda c: c.isalnum(), word)
-    if word in stopwords:
-        return None
+def dot_product(vecA, vecB):
+    product = 0
+    # for a, b in vecA, vecB:
+        # product += a * b
+    for i in range(len(vecB)):
+        product += vecA[i] * vecB[i]
+    return product
+    
+def vec_length(vec):
+    length = 0
+    for el in vec:
+        length += pow(el, 2)
+    return math.sqrt(length)
+    
+        
+def vec_similarity(vec1, vec2):
+    length_product = vec_length(vec1) * vec_length(vec2)
+    if length_product == 0:
+        return 0
     else:
-        return stemmer.stem(word, 0, len(word)-1)
+        return dot_product(vec1, vec2) / length_product
 
-def clean_word_list(list):
-    return [word for word in list if word]
+class TFIDF_Search(object):
 
+    stemmer = PorterStemmer()    
+    
+    def __init__(self, stopwords_filepath, keywords_filepath, documents_filepath):
+        self.keywords = OrderedDict()
+        self.documents = defaultdict(list)
+        self.stopwords = []
+        self.bag_of_words = {}   
+        self.documents_count = {}
+        self.read_stopwords(stopwords_filepath)
+        self.read_keywords(keywords_filepath)
+        self.read_documents(documents_filepath)
+        self.append_bag_of_words(self.documents)
+        
+    def clean_word(self, word):
+        word = word.lower()
+        word = filter(lambda c: c.isalnum(), word)
+        if word in self.stopwords:
+            return None
+        else:
+            return self.stemmer.stem(word, 0, len(word)-1)
+
+    def clean_word_list(self, list):
+        return [word for word in list if word]
+
+    def read_stopwords(self, stopwords_filepath):
+        self.stopwords = []
+        with open(stopwords_filepath) as file:
+            for line in file:
+                self.stopwords.append(line.strip())
+        self.stopwords = frozenset(self.stopwords)
+
+    def read_keywords(self, keywords_filepath):
+        self.keywords = OrderedDict()
+        with open(keywords_filepath) as file:
+            for line in file:
+                word = line.strip()
+                stemmed_word = self.clean_word(word)
+                if stemmed_word is not None:
+                    self.keywords[stemmed_word] = 0
+
+    #for k in sorted(keywords):
+    #    print k
+
+    def read_documents(self, documents_filepath):
+        self.documents = defaultdict(list)
+        with open(documents_filepath) as file:
+            current_doc = None
+            for line in file:
+                if current_doc is None:
+                    current_doc = line.strip()
+                words = line.split()
+                if words:
+                    self.documents[current_doc] += words
+                else:
+                    current_doc = None
+
+        for k, doc in self.documents.iteritems():
+            clean_words = map(self.clean_word, doc)
+            self.documents[k] = self.clean_word_list(clean_words)
+            
+    def append_bag_of_words(self, documents):
+        for keyword in self.keywords:
+            self.documents_count.setdefault(keyword, 0)
+            for k, doc in documents.iteritems():
+                keyword_count = doc.count(keyword)
+                self.bag_of_words.setdefault(k, {})[keyword] = keyword_count
+                if keyword_count > 0:
+                    self.documents_count[keyword] += 1
+        
+        
+    def tf(self, document, term):
+        n = self.bag_of_words[document][term]
+        if n == 0:
+            return 0
+        else:
+            return float(n) / max(self.bag_of_words[document].itervalues())
+            
+    def idf(self, term):
+        if self.documents_count[term] == 0:
+            return 0
+        else:
+            return math.log(len(self.documents) / self.documents_count[term])
+        
+    def tfidf(self, document):
+        result_vector = []
+        for term in sorted(self.keywords):
+            result_vector.append(self.tf(document, term) * self.idf(term))
+        return result_vector
+
+    def similarity(self, doc1, doc2):
+        #print "comparing documents: " + doc1 + " " + doc2
+        return vec_similarity(self.tfidf(doc1), self.tfidf(doc2))
+        
+    def search(self, question):
+        question_words = question.split()
+        question_words_stemmed = []
+        for word in question_words:
+            word = word.strip()
+            stemmed_word = self.clean_word(word)
+            if (stemmed_word):    
+                question_words_stemmed.append(stemmed_word)
+        self.append_bag_of_words({"question" : question_words_stemmed})
+        ranking = {}
+        for title in self.documents.iterkeys():
+            ranking[title] = self.similarity(title, "question")
+        
+        for title, sim in sorted(ranking.items(), key=(lambda t: t[1])):
+            print "%4f\t%s" % (sim, title)
+
+#for doc in documents.items():
+    #print doc
+    
 stopwords_filepath = "data/stopwords.txt"
 keywords_filepath = "data/keywords.txt"
 documents_filepath = "data/documents.txt"
 
-stopwords = []
-with open(stopwords_filepath) as file:
-    for line in file:
-        stopwords.append(line.strip())
-stopwords = frozenset(stopwords)
+searcher = TFIDF_Search(stopwords_filepath, keywords_filepath, documents_filepath)
 
-keywords = OrderedDict()
-with open(keywords_filepath) as file:
-    for line in file:
-        word = line.strip()
-        stemmed_word = clean_word(word)
-        if stemmed_word is not None:
-            keywords[stemmed_word] = 0
-
-for k in sorted(keywords):
-    print k
-
-documents = defaultdict(list)
-with open(documents_filepath) as file:
-    current_doc = None
-    for line in file:
-        if current_doc is None:
-            current_doc = line.strip()
-        words = line.split()
-        if words:
-            documents[current_doc] += words
-        else:
-            current_doc = None
-
-for k, doc in documents.iteritems():
-    clean_words = map(clean_word, doc)
-    documents[k] = clean_word_list(clean_words)
-
-for doc in documents.items():
-    print doc
+if(len(sys.argv) > 1):
+    question = sys.argv[1]
+else:
+    question = "Radial Neural networks"
+searcher.search(question)
