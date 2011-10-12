2250eee56886fb67ffc363eae6f8d479ee2a38eb
Krzysztof Urban <krz.urb@gmail.com>
2011-10-12 19:40:52 +0200

    tests for cleaner

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 import unittest
 
 from data import Vector
+from word import Cleaner
 
 class TestVectorFunctions(unittest.TestCase):
 
@@ -37,6 +38,32 @@ class TestVectorFunctions(unittest.TestCase):
         actual = Vector.similarity(self.v1, self.v2)
         expected = 70.0 / (5.477225575 * 13.190905958)
         self.assertAlmostEqual(actual, expected)
+
+class TestCleaner(unittest.TestCase):
+
+    def setUp(self):
+        stopwords = "stop halt basta".split()
+        self.c = Cleaner(stopwords)
+
+    def test_clean_word(self):
+        word = "STop"
+        actual = self.c.clean_word(word)
+        self.assertIsNone(actual)
+
+        word = "co.mp%&*uTEr"
+        actual = self.c.clean_word(word)
+        self.assertEqual(actual, "comput")
+
+    def test_clean_wordlist(self):
+        words = "stop coMputer #$%&*".split()
+        actual = self.c.clean_wordlist(words)
+        expected = ["comput"]
+        self.assertEqual(actual, expected)
+
+        words = "stop computer halt 12-10-2010 morning".split()
+        actual = self.c.clean_wordlist(words)
+        expected = ["comput", "12102010", "morn"]
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
